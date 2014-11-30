111ed79bede3c8c71edb19902067e662edd31bd3
Marcello Acar <marcello.acar@gmail.com>
2014-11-30 16:30:23 -0200

    implementado Pecas, PecasControl e FormProcurarCarro

---
+++ src/Control/PecasControl.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+import Model.Pecas;
+import Control.ConexaoBD;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+/**
+ *
+ * @author marcello
+ */
+public class PecasControl {
+    ConexaoBD con;
+    Pecas retPeca;
+    
+    public PecasControl(){
+        con = new ConexaoBD();
+    }
+    
+    public boolean setPeca(Pecas aux, Integer codigo){
+        ResultSet rs = null;
+        String texto_consulta = "INSERT INTO PECA VALUES('" + codigo + "','"
+                + aux.getDescricao() + "','" + aux.getGarantia()+ ");";
+                
+        System.out.println(texto_consulta);
+        
+        try{
+            con.st.execute(texto_consulta);
+        }catch(SQLException e){
+            return false;
+        }
+        
+        return true;        
+    }
+    
+    public ArrayList getPecas(){
+        ArrayList<Pecas> pecas = new ArrayList<Pecas>();
+        Pecas aux;
+
+        ResultSet rs = null;
+        String texto_consulta = "SELECT NOME, VENCIMENTO FROM PECA" ;
+        
+        System.out.println(texto_consulta);
+        
+        try {
+            con.st.execute(texto_consulta);
+            rs = con.st.getResultSet();
+            rs.next();
+            
+            while(rs.isAfterLast() == false){
+                aux = new Pecas();
+                aux.setDescricao(rs.getString(1));
+                aux.setGarantia(rs.getInt(2));
+                   
+                pecas.add(aux);
+                
+                rs.next();
+              }
+
+            return pecas;
+        } catch (SQLException e) {
+            return null;
+        }
+
+    }
+}
---
+++ src/Model/Pecas.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author marcello
+ */
+public class Pecas {
+    private String descricao;
+    private Integer garantia;
+    
+    public Integer getGarantia() {
+        return garantia;
+    }
+
+    public void setGarantia(Integer garantia) {
+        this.garantia = garantia;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+}
